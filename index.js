c97989246aec0a6b62d34f0bdcb38449661dd64d
Austin Baik <baik.austin@gmail.com>
2021-12-19 21:08:15 -0800

    commit tree 464fdcf58a908ae9c5093ce293afd4f4b61ada59

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+
+
+{
+let multiply 
+let num1 = 2
+let num2 = 31
+
+multiply = num1 * num2;
+}
+
+{
+    let random 
+    random = Math.ceil(Math.random())
+
+}
+
+{
+    let mod 
+    let num3 = 4 
+    let num4 = 5
+
+    mod = num3 % num4;
+}
+
+{
+    const max = 20
+
+    Math.max(max);
+}
